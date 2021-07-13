efe7daa07c0f9dc94c8da31e68c9383fa7eebf86
wrmk <elmakan@gmail.com>
2021-07-13 20:30:41 +0300

    open uri

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -1,13 +1,20 @@
 require "nokogiri"
 require "byebug"
-require "net/http"
+require "open-uri"
 
 def scraper
-  value = "ruby"
-  url = URI ("https://rabota.yandex.ru/rossiya/vakansii/?text=#{value}")
-  unparsed_page = Net::HTTP.get(url)
+  # value = "ruby"
+  url = URI ("https://www.upwork.com/ab/find-work/")
+  unparsed_page = open(url).read{|result| result.read}
   parsed_page = Nokogiri::HTML(unparsed_page)
-  job_listings = parsed_page.css('div.click-area')
+  # job_listings = parsed_page.css('div.click-area')
+  # job_listings.each do |job_listing|
+  #   job = {
+  #     title: job_listing.css('heading heading_level_3').text,
+  #     company:job_listing.css('serp-vacancy__company').text
+  #   }
+    
+  # end  
   byebug
 end
 
