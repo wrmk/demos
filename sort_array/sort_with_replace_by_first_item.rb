5b2cd7f840a85d03764bcb102143784ea8845645
wrmk <elmakan@gmail.com>
2021-09-07 21:10:44 +0300

    Update sort_with_replace_by_first_item.rb

--- sort_array/sort_with_replace_by_first_item.rb
+++ sort_array/sort_with_replace_by_first_item.rb
@@ -3,30 +3,25 @@ array= [12,30,45,9,3,15,2,8]
 def func(arr) 
   arr.size.times do |x|
     if arr[0] >= arr[x] then
-    for y in (1..(x-1)) do
-      replace(arr,0,y)
-      p arr
-    end
-    replace(arr,0,x)
-  p arr
-  elsif arr[x]<arr[x-1] then
-    for z in (1..(x-1)) do
-      if arr[x] <= arr[z] then
-        replace(arr,0,x)
-        p arr
-        for k in (z..x) do
-          replace(arr,0,k)
-          p arr
-        end
-        break
+      for y in (1..x) do
+        replace(arr,0,y)
       end
-    end
-          
+    elsif arr[x]<arr[x-1] then
+      for z in (1..(x-1)) do
+        if arr[x] <= arr[z] then
+          replace(arr,0,x)
+          for k in (z..x) do
+            replace(arr,0,k)
+          end
+          break
+        end
+      end     
     end
   end
 end
 
 def replace(arr,elem1,elem2)
   arr[elem1], arr[elem2] = arr[elem2], arr[elem1]
+  p arr
 end
 p func(array)
