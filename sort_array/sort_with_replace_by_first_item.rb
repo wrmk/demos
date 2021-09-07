2178b23fb882557ef6fc6ad49d0023f3e906118f
wrmk <elmakan@gmail.com>
2021-09-07 21:21:09 +0300

    Update sort_with_replace_by_first_item.rb

--- sort_array/sort_with_replace_by_first_item.rb
+++ sort_array/sort_with_replace_by_first_item.rb
@@ -2,11 +2,11 @@ array= [12,30,45,9,3,15,2,8]
 
 def func(arr) 
   arr.size.times do |x|
-    if arr[0] >= arr[x] then
+    if arr[0] >= arr[x] then # меняем все элементы местами через нулевой по элемент х
       for y in (1..x) do
         replace(arr,0,y)
       end
-    elsif arr[x]<arr[x-1] then
+    elsif arr[x]<arr[x-1] then #вычисляем какой элемент z больше х, меняем х и нулевой, и меняем всё местами в интервале z..x через нулевой
       for z in (1..(x-1)) do
         if arr[x] <= arr[z] then
           replace(arr,0,x)
