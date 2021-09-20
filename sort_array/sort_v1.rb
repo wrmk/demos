99ce0ea7ee58864986998de2f2374cd67aed615b
wrmk <elmakan@gmail.com>
2021-09-20 21:16:27 +0300

    refactoring

--- sort_array/sort_v1.rb
+++ sort_array/sort_v1.rb
@@ -3,15 +3,15 @@ array= [12,30,45,9,3,15,2,8]
 
 def func(arr) 
   arr.size.times do |x|
-    if arr[0] >= arr[x] then # replace in interval all elem 
-      for y in (1..x) do
+    if arr[0] >= arr[x] # replace in interval all elem 
+      for y in (1..x)
         replace(arr,y)
       end
-    elsif arr[x]<arr[x-1] then #check position where we have to put elem,replace elem and 0 position and replace all items in 1..x position
-      for z in (1..(x-1)) do
-        if arr[x] <= arr[z] then
+    elsif arr[x]<arr[x-1] #check position where we have to put elem,replace elem and 0 position and replace all items in 1..x position
+      for z in (1...x)
+        if arr[x] <= arr[z]
           replace(arr,x)
-          for k in (z..x) do
+          for k in (z..x) 
             replace(arr,k)
           end
           break
