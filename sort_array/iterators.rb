2e94e493e1bb9c05dc6ae92bc0f04833b3e43c37
wrmk <elmakan@gmail.com>
2021-10-12 20:31:59 +0300

    Update iterators.rb

--- sort_array/iterators.rb
+++ sort_array/iterators.rb
@@ -20,13 +20,32 @@
 # arr.select!{|elem| elem.chars[0] == 'с'}
 # p arr
 
-arr = %w[январь февраль март апрель май июнь июль август сентябрь октябрь ноябрь декабрь]
+# arr = %w[январь февраль март апрель май июнь июль август сентябрь октябрь ноябрь декабрь]
+# arr2 = arr.reduce do |summ,elem| 
+#   if summ.chars.size < elem.chars.size
+#     summ 
+#   else
+#     elem
+#   end
+# end
+# p arr2
 
-arr2 = arr.reduce do |summ,elem| 
-  if summ.chars.size < elem.chars.size
-    summ 
-  else
-    elem
-  end
-end
-p arr2
+# class User
+#   attr_accessor :score
+#   def set_name(name)
+#     @name = name
+#   end
+#   def set_score(score)
+#     @score = score
+#   end
+# end
+# require 'securerandom'
+# users_array = []
+# 10.times do
+#   user = User.new
+#   user.set_name(SecureRandom.alphanumeric(5))
+#   user.set_score(rand(1..5))
+#   users_array << user
+# end
+# average_score = users_array.reduce(0){|total_score,user| total_score + user.score}/10
+# users_array.each{|user| p user if user.score > average_score}
