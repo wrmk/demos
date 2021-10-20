e111e22e7919690d08ae65d6666de84a3efbeee8
wrmk <elmakan@gmail.com>
2021-10-20 20:50:18 +0300

    select iterator through each_with_object

--- sort_array/blocks.rb
+++ sort_array/blocks.rb
@@ -1,5 +1,11 @@
-arr = [2,5]
-def arr.my_map(&block) # analog map iterator
-   self.reduce([]){|arr2, x| arr2 << (yield x)}
+# arr = [2,5]
+# def arr.my_map(&block) # analogue map iterator
+#    self.reduce([]){|arr2, x| arr2 << (yield x)}
+# end
+# p arr.my_map{|x| x*x}
+
+arr = [2,5,1,15]
+def arr.my_select(&block) #analogue select iterator
+   self.each_with_object([]){|x, arr2| arr2 << x if yield x}
 end
-p arr.my_map{|x| x*x}
+p arr.my_select{|x| x > 2}
