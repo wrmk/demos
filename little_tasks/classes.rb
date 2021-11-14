d84b05bbf3ef51e26a99020d08edacd08f01afcc
wrmk <elmakan@gmail.com>
2021-11-14 20:53:56 +0300

    add methods to Integer

--- little_tasks/classes.rb
+++ little_tasks/classes.rb
@@ -1,38 +1,52 @@
-class Factory
+# class Factory
 
-  attr_reader :total, :offers
-  
-  def initialize
-    @total = 0
-    @offers = {teddy_bear: 0, ball: 0, cube: 0} 
-  end
+#   attr_reader :total, :offers
 
-  def build(arg)
-    @total += 1
-    @offers[arg] +=1
-    case arg
-    when :teddy_bear then TeddyBear.new
-    when :ball then Ball.new
-    when :cube then Cube.new
-    end
-  end
+#   def initialize
+#     @total = 0
+#     @offers = {teddy_bear: 0, ball: 0, cube: 0} 
+#   end
+
+#   def build(arg)
+#     @total += 1
+#     @offers[arg] +=1
+#     case arg
+#     when :teddy_bear then TeddyBear.new
+#     when :ball then Ball.new
+#     when :cube then Cube.new
+#     end
+#   end
 
 
-  class TeddyBear
+#   class TeddyBear
+#   end
+#   class Ball
+#   end
+#   class Cube
+#   end
+# end
+
+# factory = Factory.new
+
+# factory.build(:cube)
+# factory.build(:cube)
+# factory.build(:cube)
+# factory.build(:ball)
+# factory.build(:teddy_bear)
+
+# p factory.total
+# p factory.offers
+
+class Integer
+  def minutes
+    self * 60
   end
-  class Ball
+  def hours
+    self * 3600
   end
-  class Cube
+  def days
+    self * 86400
   end
 end
 
-factory = Factory.new
-
-factory.build(:cube)
-factory.build(:cube)
-factory.build(:cube)
-factory.build(:ball)
-factory.build(:teddy_bear)
-
-p factory.total
-p factory.offers
+p 25.minutes
