43794d96cb5a115b1539d03ebab3e4f16a493021
wrmk <elmakan@gmail.com>
2021-11-22 21:52:03 +0300

    Create get_pairs.rb

---
+++ little_tasks/get_pairs.rb
@@ -0,0 +1,20 @@
+def get_pairs(arr,sum) # выводит новый массив с парами значений, равными аргументу
+  arr2=[]
+  (arr.size - 1).times do
+    for index in 1...arr.size do
+      if (arr[0] + arr[index]) == 5
+        arr2 << [arr[0],arr[index]]
+        arr.delete_at(0)
+        arr.delete_at(index-1)
+        break
+      elsif index == arr.size - 1
+        arr.delete_at(0)
+      end
+    end
+  end
+  arr2
+end
+
+p get_pairs([-5,10,2,2,3,5,0,10,3],5)
+p get_pairs([22,3,5,0,2,2],5)
+p get_pairs([5,5,5,0,0,0,5],5)
