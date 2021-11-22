584955d9d226a886714b6e27b4639214e0c14301
wrmk <elmakan@gmail.com>
2021-11-22 22:46:49 +0300

    add arg sum

--- little_tasks/get_pairs.rb
+++ little_tasks/get_pairs.rb
@@ -2,7 +2,7 @@ def get_pairs(arr,sum) # выводит новый массив с парами 
   arr2=[]
   (arr.size - 1).times do
     for index in 1...arr.size do
-      if (arr[0] + arr[index]) == 5
+      if (arr[0] + arr[index]) == sum
         arr2 << [arr[0],arr[index]]
         arr.delete_at(0)
         arr.delete_at(index-1)
