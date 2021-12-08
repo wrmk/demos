48574e68b0ca3a8d064a3df0aa4ea301d00dfba5
wrmk <elmakan@gmail.com>
2021-12-08 20:15:03 +0300

    shuffle array

---
+++ little_tasks/shuffle.rb
@@ -0,0 +1,17 @@
+def permutation(arr)
+  #   permutation([1, 2]) => [1, 2]
+  #   permutation([1, 2]) => [2, 1]
+  #   permutation([1, 2, 3]) => [2, 1, 3]
+    arr_copy = arr.dup
+    arr2= []
+
+    while arr_copy.size > 0
+        random = rand(0...arr_copy.size)
+        arr2 << arr_copy[random]
+        arr_copy.delete_at(random)
+    end
+    arr2
+end
+
+arr = [8,7,4,1,5,9]
+p permutation(arr)
