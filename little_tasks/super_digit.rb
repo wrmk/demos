c351298b5f26b066378529f73eb84e25229793ac
wrmk <elmakan@gmail.com>
2021-12-10 19:27:09 +0300

    refactor, add method without recursive

--- little_tasks/super_digit.rb
+++ little_tasks/super_digit.rb
@@ -1,12 +1,21 @@
-def super_digit(val)#
-  val2 = val.to_s.chars.reduce(0){|res,elem| res + elem.to_i}
-  if val2 > 9
-    val2 = super_digit(val2)
+def super_digit1(val)
+  val = val.digits.sum
+  if val > 9
+    val = super_digit1(val)
   end
-  val2
+  val
 end
+
+def super_digit2(val)
+  while val > 9
+    val = val.digits.sum
+  end
+  val
+end
+
+
 #super_digit(1) = 1
 #super_digit(10) = 1 + 0 = 1
 #super_digit(12345) = 1 + 2 + 3 + 4 + 5 == 15 =>  1 + 5 = 6
 
-p super_digit(12345)
+p super_digit1(123456)
