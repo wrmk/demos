967abe5c84034aa233fddb294b4af166bb2c85b6
wrmk <elmakan@gmail.com>
2021-12-10 21:22:46 +0300

    Update inheritance.rb

--- little_tasks/inheritance.rb
+++ little_tasks/inheritance.rb
@@ -1,17 +1,45 @@
-class Ship
-end
-class CivilShip < Ship
-  attr_accessor :hold, :crane
-end
-class WarShip < Ship
-  attr_accessor :rockets, :torpedoes
-end
+# class Ship
+# end
+# class CivilShip < Ship
+#   attr_accessor :hold, :crane
+# end
+# class WarShip < Ship
+#   attr_accessor :rockets, :torpedoes
+# end
+# submarine = WarShip.new
+# dry_cargo = CivilShip.new
+# container = CivilShip.new
+# container.instance_eval ('undef :hold, :hold=')
+# p container.methods
 
-submarine = WarShip.new
-dry_cargo = CivilShip.new
-container = CivilShip.new
-container.instance_eval ('undef :hold, :hold=')
+
+# class User
+#   attr_accessor :name, :surname, :email
+# end
+# class Guest < User
+# end
+
+# class Moderator < User
+#   def say
+#     self.class
+#   end
+#   alias to_s say
+# end
+# moderator = Moderator.new
+# p moderator.say
 
 
-p container.methods
-
+# class Person
+#   attr_accessor :name, :surname
+# end
+# class User < Person
+# end
+# user = User.new
+# user.name = 'bob'
+# p user.name
+# class Person
+#   def self.new
+#     'restrict to create'
+#   end
+# end
+# p Person.new
