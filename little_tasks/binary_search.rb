cff38c845cc740343b56e846cf7fffb18f4eafd3
wrmk <elmakan@gmail.com>
2021-12-20 20:46:49 +0300

    recursive binary search

--- little_tasks/binary_search.rb
+++ little_tasks/binary_search.rb
@@ -17,5 +17,23 @@ def search(arr,val)
 
   'there no such value'
 end
+
+
+def recursive_search(arr,val)
+  min = 0
+  max = arr.size - 1
+  mid = (min + max)/2
+
+  if arr[mid] == val
+    return 'value exist'
+  elsif max == min
+    return 'there no such value'
+  elsif arr[mid] > val
+    recursive_search(arr[min...mid],val)
+  else
+    recursive_search(arr[(mid+1)..max],val)
+  end
+end
+
 range = (1..153).to_a
-p search(range, 3)
+p recursive_search(range, 153)
