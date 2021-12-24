f3f90f2da56bcf60ab26ee1a72a9e7061c5bf257
wrmk <elmakan@gmail.com>
2021-12-24 10:58:43 +0300

    benchmark quick sort

--- little_tasks/quick_sort.rb
+++ little_tasks/quick_sort.rb
@@ -1,21 +1,22 @@
-def quick_sort(arr)
+require 'benchmark'
 
+def quick_sort(arr)
   if arr.size < 2
     return arr
   end
 
   pivot = arr.delete_at(arr.size/2)
-
   low, high = arr.partition{|val| val <= pivot}
   low = quick_sort(low)
   high = quick_sort(high)
 
   low + [pivot] + high
-
 end
 
-arr = Array.new(20){rand(100)}
-p arr
-p quick_sort(arr)
+arr = Array.new(100000){rand(1000)}
+
+p Benchmark.measure{quick_sort(arr)}
+p Benchmark.measure{arr.sort!}
+
 
 
