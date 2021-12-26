61eb4ae9a918c58eacaf0cf29c3a237ce1fe93d3
wrmk <elmakan@gmail.com>
2021-12-26 20:00:18 +0300

    refactoring

--- little_tasks/merge_sort.rb
+++ little_tasks/merge_sort.rb
@@ -1,8 +1,6 @@
 def merge_sort(arr)
-  if arr.size < 2
-    return arr
-  elsif arr.size < 3
-    arr[0], arr[1] = arr[1], arr[0] if arr[1] < arr[0]
+  if arr.size < 3
+    arr[0], arr[1] = arr[1], arr[0] if arr.size == 2 && arr[1] < arr[0]
     return arr
   end
 
@@ -12,10 +10,12 @@ def merge_sort(arr)
   summary = []
 
   while left.size > 0 || right.size > 0
-    if left.size > 0 && right.size > 0
-      summary << (left[0] < right[0] ? left.delete_at(0) : right.delete_at(0))
-    else
-      summary << (left.delete_at(0) || right.delete_at(0))
+    summary << begin
+      if left.size > 0 && right.size > 0
+        left[0] < right[0] ? left.delete_at(0) : right.delete_at(0)
+      else
+        left.delete_at(0) || right.delete_at(0)
+      end
     end
   end
   summary
