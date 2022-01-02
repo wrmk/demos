bf95a83be31df3dc904761bd510d139c992efa55
wrmk <elmakan@gmail.com>
2022-01-02 20:04:50 +0300

    add edit and show methods

--- little_tasks/namespace.rb
+++ little_tasks/namespace.rb
@@ -28,13 +28,25 @@ class Unit
     @employees = []
   end
 
+  def show(role: nil)
+    if role
+      list = employees.select{|employee| employee.role == role}
+    else
+      list = employees.sort_by{|employee| employee.name}
+    end
+    list.each{|employee| p "#{employee.name} is #{employee.role}"}
+  end
+
   def add(role:, name:)
-    employee = Employee.new(role: role,name: name)
-    @employees << employee
+    employees << Employee.new(role: role,name: name)
   end
 
   def remove(name:)
-    @employees.delete_if{|employee| employee.name == name }
+    employees.delete_if{|employee| employee.name == name }
+  end
+
+  def edit(role:, name:)
+    employees.find{|employee| employee.name == name}.role = role
   end
 
   class Employee
@@ -43,8 +55,8 @@ class Unit
       @role = role
       @name = name
     end
-
   end
+
 end
 
 unit = Unit.new
@@ -52,3 +64,11 @@ unit.add(role: 'teamlead', name:'John')
 p unit.employees
 unit.remove(name: 'John')
 p unit.employees
+unit.add(role: 'backend', name:'Zessica')
+p unit.employees
+unit.edit(role: 'frontend', name: 'Zessica')
+unit.add(role: 'teamlead', name:'Bohn')
+unit.add(role: 'backend', name:'Bratt')
+p unit.employees
+unit.show
+unit.show(role: 'backend')
