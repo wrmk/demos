77d531968bf089014e0b703e9b3fd36e48699afb
wrmk <elmakan@gmail.com>
2022-01-05 23:44:18 +0300

    Update benchmark.rb

--- little_tasks/benchmark.rb
+++ little_tasks/benchmark.rb
@@ -2,9 +2,14 @@ require 'benchmark'
 require_relative 'quick_sort'
 require_relative 'merge_sort'
 
+class Array
+  include QuickSort
+end
 
-arr = (0..100000).to_a.shuffle
 
-p Benchmark.measure{quick_sort(arr)}
+arr = (0..1_000_000).to_a.shuffle
+# p arr.quick_sort
 p Benchmark.measure{merge_sort(arr)}
+p Benchmark.measure{merge_sort_v2(arr)}
+p Benchmark.measure{arr.quick_sort}
 
