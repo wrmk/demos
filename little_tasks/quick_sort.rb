e8671800d69c30834b69482584b27dc7a807daa5
wrmk <elmakan@gmail.com>
2022-01-05 23:42:33 +0300

    module QuickSort to include into Array class

--- little_tasks/quick_sort.rb
+++ little_tasks/quick_sort.rb
@@ -1,14 +1,14 @@
-def quick_sort(arr)
-  if arr.size < 2
-    return arr
+module QuickSort
+  def quick_sort(arr = self)
+    if arr.size < 2
+      return arr
+    end
+
+    pivot = arr.delete_at(rand(0...arr.size))
+    low, high = arr.partition{|val| val < pivot}
+    low = quick_sort(low)
+    high = quick_sort(high)
+
+    low + [pivot] + high
   end
-
-  pivot = arr.delete_at(rand(0...arr.size))
-  low, high = arr.partition{|val| val < pivot}
-  low = quick_sort(low)
-  high = quick_sort(high)
-
-  low + [pivot] + high
 end
-
-
