1fd5bc77b48708997207f4469e24574406a1fa1d
wrmk <elmakan@gmail.com>
2022-01-06 13:20:39 +0300

    quick sort with &block

--- little_tasks/quick_sort.rb
+++ little_tasks/quick_sort.rb
@@ -11,4 +11,17 @@ module QuickSort
 
     low + [pivot] + high
   end
+
+  def quick_sort_v2(&block)
+    if self.size < 2
+      return self
+    end
+
+    pivot = self.delete_at(rand(0...self.size))
+    left, right = self.partition{|val| (block.call val) < (block.call pivot)}
+    left = left.quick_sort_v2(&block)
+    right = right.quick_sort_v2(&block)
+
+    left + [pivot] + right
+  end
 end
