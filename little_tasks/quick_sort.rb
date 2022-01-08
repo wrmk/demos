71a2afdf046e93b3f8568c06beb2842bcd1459d7
wrmk <elmakan@gmail.com>
2022-01-08 15:17:30 +0300

    remove function without block

--- little_tasks/quick_sort.rb
+++ little_tasks/quick_sort.rb
@@ -1,18 +1,5 @@
 module QuickSort
-  def quick_sort
-    if self.size < 2
-      return self
-    end
-
-    pivot = self.delete_at(rand(0...self.size))
-    low, high = self.partition{|val| val < pivot}
-    low = low.quick_sort
-    high = high.quick_sort
-
-    low + [pivot] + high
-  end
-
-  def quick_sort_v2(&block)
+  def quick_sort(&block)
     if self.size < 2
       return self
     end
@@ -25,8 +12,8 @@ module QuickSort
         val < pivot
       end
     end
-    left = left.quick_sort_v2(&block)
-    right = right.quick_sort_v2(&block)
+    left = left.quick_sort(&block)
+    right = right.quick_sort(&block)
 
     left + [pivot] + right
   end
