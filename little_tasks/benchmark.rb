08cc59fa129ce288a466bf1dc8e179a13496e59f
wrmk <elmakan@gmail.com>
2022-01-08 20:36:38 +0300

    refine Array in module, and use in benchmark

--- little_tasks/benchmark.rb
+++ little_tasks/benchmark.rb
@@ -2,14 +2,15 @@ require 'benchmark'
 require_relative 'quick_sort'
 require_relative 'merge_sort'
 
-Array.include QuickSort
+using QuickSort
+# Array.include QuickSort
 
 
 arr = (0..1_00).to_a.shuffle
 arr2 = Array.new(100){Array('a'..'z').sample(rand(1..25)).join}
 
-p arr2.quick_sort_v2{|val| val.size}
-# p arr.quick_sort_v2{|val| -val}
+# p arr2.quick_sort{|val| -val.size}
+p arr.quick_sort{|val| val}
 # p Benchmark.measure{merge_sort(arr)}
 # p Benchmark.measure{merge_sort_v2(arr)}
 # p Benchmark.measure{arr.quick_sort}
--- little_tasks/quick_sort.rb
+++ little_tasks/quick_sort.rb
@@ -1,24 +1,26 @@
 module QuickSort
-  def quick_sort(&block)
-    if self.size < 2
-      return self
-    end
-
-    pivot = self.delete_at(rand(0...self.size))
-    left, right = self.partition do |val|
-      if block_given?
-        (block.call val) < (block.call pivot)
-      else
-        val < pivot
+  refine Array do
+    def quick_sort(&block)
+      if self.size < 2
+        return self
       end
+
+      pivot = self.delete_at(rand(0...self.size))
+      left, right = self.partition do |val|
+        if block_given?
+          (block.call val) < (block.call pivot)
+        else
+          val < pivot
+        end
+      end
+      left.quick_sort!(&block)
+      right.quick_sort!(&block)
+
+      left + [pivot] + right
     end
-    left.quick_sort!(&block)
-    right.quick_sort!(&block)
 
-    left + [pivot] + right
-  end
-
-  def quick_sort!(&block)
-    self.replace(quick_sort(&block))
+    def quick_sort!(&block)
+      self.replace(quick_sort(&block))
+    end
   end
 end
