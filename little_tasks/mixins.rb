b61da51ba781ae69e294e4517bdb135a1926186d
wrmk <elmakan@gmail.com>
2022-01-23 19:54:34 +0300

    module RomanNumbers

--- little_tasks/mixins.rb
+++ little_tasks/mixins.rb
@@ -11,26 +11,49 @@
 
 # p red
 
-module Fivable
-  def self.included(cls)
-    cls.class_eval do
-      def initialize
-        @@count ||= 0
-        @@count += 1
-        raise "no more 5 instances" if @@count > 5
-      end
+# module Fivable
+#   def self.included(cls)
+#     cls.class_eval do
+#       def initialize
+#         @@count ||= 0
+#         @@count += 1
+#         raise "no more 5 instances" if @@count > 5
+#       end
+#     end
+#   end
+# end
 
+# class Some
+#   include Fivable
+# end
+
+# some1 = Some.new
+# some2 = Some.new
+# some3 = Some.new
+# some4 = Some.new
+# some5 = Some.new
+# some6 = Some.new
+
+module RomanNumbers
+  def roman
+    case self
+    when 0 then "N"
+    when 1 then "I"
+    when 2 then "II"
+    when 3 then "III"
+    when 4 then "IV"
+    when 5 then "V"
+    when 6 then "VI"
+    when 7 then "VII"
+    when 8 then "VIII"
+    when 9 then "IX"
+    else "this is not a digit"
     end
   end
 end
 
-class Some
-  include Fivable
+class Integer
+  include RomanNumbers
 end
 
-some1 = Some.new
-some2 = Some.new
-some3 = Some.new
-some4 = Some.new
-some5 = Some.new
-some6 = Some.new
+p 5.roman
