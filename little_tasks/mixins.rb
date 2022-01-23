d830dbf42522cf04914f41eeaa4838a0799053b9
wrmk <elmakan@gmail.com>
2022-01-23 19:43:52 +0300

    fivable module

--- little_tasks/mixins.rb
+++ little_tasks/mixins.rb
@@ -1,12 +1,36 @@
-COLORS = {
-  red: 'красный',
-  orange: 'желтый'
-}
+# COLORS = {
+#   red: 'красный',
+#   orange: 'желтый'
+# }
 
-COLORS.each do |key,value|
-  define_method key do
-    value
+# COLORS.each do |key,value|
+#   define_method key do
+#     value
+#   end
+# end
+
+# p red
+
+module Fivable
+  def self.included(cls)
+    cls.class_eval do
+      def initialize
+        @@count ||= 0
+        @@count += 1
+        raise "no more 5 instances" if @@count > 5
+      end
+
+    end
   end
 end
 
-p red
+class Some
+  include Fivable
+end
+
+some1 = Some.new
+some2 = Some.new
+some3 = Some.new
+some4 = Some.new
+some5 = Some.new
+some6 = Some.new
