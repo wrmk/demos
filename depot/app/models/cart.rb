448f5b8a013664597a7a154f45f92c44d32ff46b
Vladimir Bikmeev <elmakan@gmail.com>
2022-04-07 23:07:12 +0300

    Delete depot directory

--- depot/app/models/cart.rb
+++
@@ -1,4 +0,0 @@
-class Cart < ApplicationRecord
-  has_many :line_items, dependent: :destroy 
-                        #если удалится корзина, удалятся и товары в этой корзине
-end
--- depot/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
-  get 'store/index'
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'store#index', as: 'store'
-end
-
